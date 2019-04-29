9c94391bb223a7c159a4207b19ad4cc643071dca
leotian <leotiann@gmail.com>
2019-04-29 14:37:31 +0800

    refactor: 抽取获取文章链接和title的代码

---
+++ posts.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const getPostList = ($) => {
+  const postList = []
+  // 获取首页标题
+  $(".post-title-link").each((i, elem) => {
+    postList.push({
+      abbrLink: $(elem).attr('href').replace('posts', '').replace(/\//g, ''),
+      postTitle: $(elem).text(),
+    })
+  })
+  return postList
+}
+
+async function getPostsByReptile(domain) {
+  let postList = []
+  // 获取总页数
+  const homePage = await axios.get(domain)
+  const postPages = cheerio.load(homePage.data)(".page-number").last().text()
+  // 获取每页的文章标题和链接
+  for (let i = 1; i <= postPages; i++) {
+    const url = i > 2 ? `${domain}/page/${i}/` : domain
+    const html = await axios.get(url)
+    const $ = cheerio.load(html.data)
+    postList = postList.concat(getPostList($))
+  }
+  return postList
+}
+
+function getPostsByDir(postDir) {
+  const htmlList = fs.readdirSync(postDir)
+
+  const postList = htmlList.map((abbrLink) => {
+    // 从本地文件获取html
+    const html = fs.readFileSync(`${postDir}/${abbrLink}/index.html`).toString()
+    // 加载html 获取title
+    const $ = cheerio.load(html)
+    const postTitle = $(".post-title").text()
+    return {
+      abbrLink,
+      postTitle,
+    }
+  })
+  return postList
+}
+
+// getPostsByReptile('https://leotian.cn')
+// getPostsByDir('/Users/tyb/workspace/blog/public/posts')
+
+module.exports = {
+  getPostsByReptile,
+  getPostsByDir,
+}
---
+++ revise.js
@@ -0,0 +1,43 @@
+const AV = require('leancloud-storage')
+const { getPostsByDir, getPostsByReptile } = require('./posts')
+
+AV.init({
+  appId: 'GxfYzf5roQxLdedcwOheEnWE-gzGzoHsz',
+  appKey: 'MomkTz6Tq1aBGWvzNDSv8o31'
+})
+
+// 通过本地目录获取
+// const postDir = '/Users/tyb/workspace/blog/public/posts'
+// const postList = getPostsByDir(postDir)
+
+// 通过爬虫获取文章标题和链接
+const domain = 'https://leotian.cn'
+const postList = getPostsByReptile(domain)
+
+Promise.resolve(postList).then(result => {
+  result.forEach((post) => {
+    // 查询同一标题下的所有记录
+    const query = new AV.Query('Counter')
+    query.equalTo('title', post.postTitle)
+
+    // 留下times最多的一个 修改url 并修改其他脏数据url
+    query.find().then((results) => {
+      let maxResult = results[0]
+      results.forEach((result) => {
+        if(result.attributes.time > maxResult.attributes.time) maxResult = result
+      })
+      maxResult.set('url', `/posts/${post.abbrLink}/`)
+      maxResult.save()
+
+      console.log(maxResult)
+
+      // 更改短链后有一些新的访问 属于脏数据 把他们的链接修改错误来解除关联关系
+      results.forEach((result) => {
+        if(result.attributes.url === maxResult.attributes.url && result.id !== maxResult.id) {
+          result.set('url', `/posts/${post.abbrLink}`)
+          result.save()
+        }
+      })
+    })
+  })
+})
